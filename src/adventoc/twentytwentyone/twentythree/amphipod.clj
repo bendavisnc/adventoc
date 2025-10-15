ba9b00732c20a89e5f869913d5f670a4b5416760
bendavisnc <bendavisnc@gmail.com>
2025-10-14 20:18:34 -0400

    Adds new start since ditching protocols.

--- src/adventoc/twentytwentyone/twentythree/amphipod.clj
+++ src/adventoc/twentytwentyone/twentythree/amphipod.clj
@@ -3,50 +3,32 @@
    [clojure.core :exclude [name]])
   (:gen-class))
 
-(defprotocol Amphipod
-  :extend-via-metadata true
-  (energy-cost [this])
-  (amphipod-name [this]))
+(def amphipod-energys {:A 1
+                       :B 10
+                       :C 100
+                       :D 1000})
 
-(def A (with-meta `A
-         {:energy-cost (fn [_] 1)
-          :amphipod-name (fn [_] "Amber")}))
+(def amphipods {::A0 {:moves []}
+                ::A1 {:moves []}
+                ::B0 {:moves []}
+                ::B1 {:moves []}
+                ::C0 {:moves []}
+                ::C1 {:moves []}
+                ::D0 {:moves []}
+                ::D1 {:moves []}})
 
-(def B (with-meta `B
-         {:energy-cost (fn [_] 10)
-          :amphipod-name (fn [_] "Bronze")}))
-
-(def C (with-meta `C
-         {:energy-cost (fn [_] 100)
-          :amphipod-name (fn [_] "Copper")}))
-
-(def D (with-meta `D
-         {:energy-cost (fn [_] 1000)
-          :amphipod-name (fn [_] "Desert")}))
-
-(defmethod print-method clojure.lang.Symbol [sym ^java.io.Writer w]
-  (if (-> sym meta :amphipod-name)
-    (.write w (name sym))
-    (.write w (str sym))))
-
-(def burrow {:hallway (vec (repeat 11 nil))
-             :room {:A [nil, nil]
-                    :B [nil, nil]
-                    :C [nil, nil]
-                    :D [nil, nil]}
-             :indexes {:A 2
-                       :B 4
-                       :C 6
-                       :D 8}})
+(defn amphipod-energy [amphipod]
+  (if-let [e (some-> amphipod name first str keyword amphipod-energys)]
+    e
+    (throw (ex-info (str "Unknown amphipod energy for : " amphipod) {:amphipod amphipod}))))
 
 (defn -main [& _]
-  (let [burrow-state (-> burrow
-                         (assoc-in [:room :A 0] A)
-                         (assoc-in [:room :A 1] B)
-                         (assoc-in [:room :B 0] D)
-                         (assoc-in [:room :B 1] C)
-                         (assoc-in [:room :C 0] C)
-                         (assoc-in [:room :C 1] B)
-                         (assoc-in [:room :D 0] A)
-                         (assoc-in [:room :D 1] D))]
-    (println burrow-state)))
+  (println (amphipod-energy ::C1)))
+
+(comment (amphipod-energys (keyword (str (first (name ::B1))))))
+
+;; #############
+;; #...........#
+;; ###B#C#B#D###
+;;   #A#D#C#A#
+;;   #########
