c73c36b694aa6ed25ad03438fcd7c1614f26ca46
bendavisnc <bendavisnc@gmail.com>
2026-01-21 16:50:57 -0500

    Removes loop.

--- src/adventoc/twentytwentyfive/seven/laboratories.clj
+++ src/adventoc/twentytwentyfive/seven/laboratories.clj
@@ -23,41 +23,30 @@
                       c))
                (map-indexed vector row))))
 
+(defn beam-insert-step [grid, [x, y]]
+  (let [top (fn [[x', y']]
+              (and (= vacancy (get-in grid [y', x']))
+                   (#{s, beam} (get-in grid [(dec y'), x']))))
+        left (fn [[x', y']]
+               (= splinter (get-in grid [y', (inc x')])))
+        right (fn [[x', y']]
+                (= splinter (get-in grid [y', (dec x')])))
+        should-insert? ((some-fn top left right) [x, y])]
+    (if should-insert?
+      (update grid y #(beam-at % x))
+      grid)))
+
+(defn all-coords [[max-x, max-y]]
+  (mapcat (fn [x]
+            (map (fn [y]
+                   [x, y])
+                 (range max-y)))
+          (range max-x)))
+
 (defn insert-beams [grid]
-  (loop [acc nil
-         [row & more] grid]
-    (if-not row
-      (reverse acc)
-      (let [start-idx        (index-of row s)
-            left-idx         (index-of row (str vacancy splinter))
-            right-idx        (index-of row (str splinter vacancy))
-            top-row          (first acc)
-            bottom-idx       (when top-row
-                               (some (fn [[i c]]
-                                       (when (= [beam vacancy] [(nth top-row i), c])
-                                         i))
-                                     (map-indexed vector row)))]
-        (cond
-          start-idx
-          (let [next-row (first more)
-                beam-row (beam-at next-row start-idx)]
-            (recur
-              (list beam-row row)
-              (rest more)))
-
-          left-idx
-          (recur acc
-                 (cons (beam-at row left-idx) more))
-          right-idx
-          (recur acc
-                 (cons (beam-at row (inc right-idx)) more))
-
-          bottom-idx
-          (recur acc
-                 (cons (beam-at row bottom-idx) more))
-          :else
-          (recur (cons row acc)
-                 more))))))
+  (let [max-x (count (first grid))
+        max-y (count grid)]
+    (reduce beam-insert-step grid (all-coords [max-x, max-y]))))
 
 (defn beam-splinters-count [grid]
   (loop [how-many 0
